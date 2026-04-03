bf4e5affc87920ce4f66d5511a478fe1cbc6dfbc
blade-of-miquella <melinaprogg@gmail.com>
2026-04-03 19:07:16 +0300

    add simple level and player logic

--- DEMO/GAME/level.py
+++ DEMO/GAME/level.py
@@ -1,4 +1,8 @@
 import pygame
+from map import get_map
+from settings import TILESIZE
+from tile import Tile
+from player import Player
 
 class Level:
     def __init__(self):
@@ -8,8 +12,19 @@ class Level:
         self.visible_sprites = pygame.sprite.Group()
         self.obstacle_sprites = pygame.sprite.Group()
 
+        self.create_map(1)
+
     def create_map(self, index):
-        pass
+        self.map = get_map(index)
+
+        for row_index, row in enumerate(self.map):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+
+                if col == 1: Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+                if col == 99: self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
-        pass
+        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
